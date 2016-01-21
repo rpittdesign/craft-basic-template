bd5dbefc6aaac4d7409d2f139a324b5b2f5925d8
Laura Montgomery <laura@armitageonline.co.uk>
2016-01-21 10:32:57 +0000

    SCSS, CSS, and config.rb (sass settings)

---
+++ public/config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Change to the name of your theme folder:
+http_path = "/"
+css_dir = "assets/css"        
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
+
+output_style = :nested
+# :nested :expanded :compact :compressed
+environment = :development
+# :development :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+color_output = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+preferred_syntax = :scss
